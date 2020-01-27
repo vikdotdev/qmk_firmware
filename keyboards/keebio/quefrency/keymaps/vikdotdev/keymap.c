cf91d13cd15036508a21b7e2118d5915f7f5af6f
vik <vik@localhost.localdomain>
2020-01-27 14:29:34 +0200

    latest config

--- keyboards/keebio/quefrency/keymaps/vikdotdev/keymap.c
+++ keyboards/keebio/quefrency/keymaps/vikdotdev/keymap.c
@@ -65,11 +65,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_60_with_macro(
-    RGB_VAD, RGB_VAI,  KC_GRV,           KC_1,   KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,               KC_EQL, KC_NO,  KC_NO, \
+    RGB_VAD, RGB_VAI,  KC_EQL,           KC_1,   KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,               KC_EQL, KC_NO,  KC_NO, \
     RGB_HUD, RGB_HUI,  LT(_FFN, KC_TAB), KC_Q,   KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,               KC_RBRC, KC_BSLS, \
-    RGB_SAD, RGB_SAI,  LCTL_T(KC_ESC),   KC_A,   KC_S,     KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,               KC_NO, \
-    RGB_MOD, RGB_RMOD, KC_LSFT,          KC_Z,   KC_X,     KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,               KC_NO, \
-    TG(_PLEB),RGB_TOG, KC_NO,            KC_CAPS,LWIN_T(KC_DEL),LALT_T(KC_BSPC),LT(_FN1,KC_SPC), LT(_FN1, KC_ENT), KC_BSPC, KC_RALT, KC_NO, KC_NO, KC_NO, KC_NO
+    RGB_SAD, RGB_SAI,  LCTL_T(KC_ESC),   KC_A,   KC_S,     KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RWIN_T(KC_QUOT),       KC_NO, \
+    RGB_MOD, RGB_RMOD, LSFT_T(KC_LBRC),  KC_Z,   KC_X,     KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_RBRC),       KC_NO, \
+    TG(_PLEB),RGB_TOG, KC_CAPS,          LWIN_T(KC_LBRC),LWIN_T(KC_RBRC),LALT_T(KC_BSPC),LT(_FN1,KC_SPC), LT(_FN1, KC_ENT), RALT_T(KC_BSPC), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
   ),
   [_FN1] = LAYOUT_60_with_macro(
     _______, _______,  KC_MUTE,          KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, KC_LPRN, KC_RPRN, _______,               _______, _______, _______, \
