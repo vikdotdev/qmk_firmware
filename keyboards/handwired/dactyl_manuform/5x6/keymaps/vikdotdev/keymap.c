b3aecd4dffc68288f35c36ecbd7fae37de4fc84b
Viktor Habchak <vikdotdev@gmail.com>
2021-04-01 18:23:32 +0300

    update keymap

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/vikdotdev/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/vikdotdev/keymap.c
@@ -6,21 +6,23 @@
 #define _FN 2
 #define _MOUSE 3
 #define _GAME 4
-#define _CSGO 5
+#define _WARBAND 5
 #define _SIDE 6
 #define LC_ESC      LCTL_T(KC_ESC)
 #define WIN_TAB     LWIN_T(KC_TAB)
+#define WIN_SLS     RWIN_T(KC_BSLS)
 #define RC_BSPC     RCTL_T(KC_BSPC)
 #define TS_ENT      LT(_SIDE, KC_ENT)
 #define OSL_FN      OSL(_FN)
 #define OSL_NUM     OSL(_NUM)
-#define OSM_WIN     OSM(KC_LWIN)
+#define OSM_LW      OSM(MOD_LGUI)
 #define OSM_LS      OSM(MOD_LSFT)
 #define OSM_LC      OSM(MOD_RCTL)
 #define OSM_LA      OSM(MOD_LALT)
+#define OSM_CS      OSM(MOD_LCTL | MOD_LSFT)
 #define TG_MOU      TG(_MOUSE)
 #define TG_GAME     TG(_GAME)
-#define TG_CSGO     TG(_CSGO)
+#define TG_WARBAND  TG(_WARBAND)
 
 float side_layer_tone[][2] = SONG(SIDE_LAYER_NOTE);
 float fn_layer_tone[][2] = SONG(FN_LAYER_NOTE);
@@ -251,33 +253,33 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_MAIN] = LAYOUT_5x6(
-     KC_1   , KC_2  , KC_3  , KC_4  , KC_5  , KC_6,                 KC_7, KC_8, KC_9,    KC_0,   KC_GRV,  RESET,
-     WIN_TAB, KC_Q  , KC_W  , KC_E  , KC_R  , KC_T,                 KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSLS,
+     KC_1   , KC_2  , KC_3  , KC_4  , KC_5  , KC_6,                 KC_7, KC_8, KC_9,    KC_0,   KC_TILD, KC_GRV ,
+     WIN_TAB, KC_Q  , KC_W  , KC_E  , KC_R  , KC_T,                 KC_Y, KC_U, KC_I,    KC_O,   KC_P,    WIN_SLS,
      LC_ESC , KC_A  , KC_S  , KC_D  , KC_F  , KC_G,                 KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
      OSM_LS,  KC_Z  , KC_X  , KC_C  , KC_V  , KC_B,                 KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, OSM_LS,
-                      KC_LBRC, KC_RBRC,                                         KC_MINS, KC_EQL,
-                                      OSL_NUM,                            OSM_LC,
-                                      SP_SPC,KC_UNDS,          TG_MOU,    TS_ENT,
-                                      OSM_LA,KC_CAPS,          OSM_WIN,   OSM_LA
+                      KC_LBRC, KC_RBRC,                                         KC_EQL,  KC_MINS,
+                                      OSL_NUM,                            TG_MOU,
+                                      SP_SPC,KC_UNDS,           OSM_LC,   TS_ENT,
+                                      OSM_LA,OSM_CS,            OSM_LW,   OSL_FN
   ),
   [_SIDE] = LAYOUT_5x6(
-     _______,_______,_______,_______,_______,_______,                      TG_GAME,TG_CSGO,_______,_______,_______,KC_NO,
-     _______,_______,_______,C_TAB_B,C_TAB_F,LW_SPC ,                      AU_TOG ,C_LBRC ,C_RBRC ,KC_NO  ,KC_PSCR,KC_PIPE,
-     KC_MUTE,KC_VOLD,KC_VOLU,KC_BSPC,KC_DEL ,KC_ENT ,                      KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,KC_COLN,KC_DQUO,
-     KC_MPLY,KC_MPRV,KC_MNXT,A_TAB_B,A_TAB_F,OSL_FN ,                      _______,_______,KC_LABK,KC_RABK,KC_QUES,_______,
-                    KC_LCBR,KC_RCBR,                                                       KC_UNDS,KC_PLUS,
+     _______,_______,_______,_______,_______,_______,                      TG_GAME,TG_WARBAND,_______,_______, AU_TOG ,RESET  ,
+     _______,_______,_______,C_TAB_B,C_TAB_F,LW_SPC ,                      _______,C_LBRC ,C_RBRC ,_______,KC_PSCR,_______,
+     KC_MUTE,KC_VOLD,KC_VOLU,KC_BSPC,KC_DEL ,KC_ENT ,                      KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,_______,_______,
+     KC_MPLY,KC_MPRV,KC_MNXT,A_TAB_B,A_TAB_F,OSL_FN ,                      _______,_______,_______,_______,_______,_______,
+                     _______,_______,                                                      _______,_______,
                                      _______,                                      _______,
                                              _______,_______,      _______,LW_SPC ,
-                                             _______,_______,      _______,_______
+                                             _______,_______,      KC_CAPS,_______
   ),
   [_NUM] = LAYOUT_5x6(
-     _______,_______,_______,_______,_______,_______,                  _______,_______,_______,_______,_______,_______,
-     _______,KC_PEQL,KC_P7 , KC_P8 , KC_P9  ,KC_PDOT,                  KC_PEQL, KC_P7 , KC_P8 , KC_P9 ,KC_PDOT,_______,
-     _______,KC_PPLS,KC_P4 , KC_P5 , KC_P6  ,KC_PAST,                  KC_PPLS, KC_P4 , KC_P5 , KC_P6 ,KC_PAST,_______,
-     _______,KC_PMNS,KC_P1 , KC_P2 , KC_P3  ,KC_PSLS,                  KC_PMNS, KC_P1 , KC_P2 , KC_P3 ,KC_PSLS,_______,
-                     _______,KC_P0 ,                                                    KC_P0 , _______,
+     KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,                  KC_NO  ,KC_PAST,KC_LCBR,KC_RCBR,KC_NO  ,KC_NO  ,
+     KC_NO  ,KC_NO  ,KC_P7 , KC_P8 , KC_P9  ,KC_P0  ,                  KC_COMM, KC_P7 , KC_P8 , KC_P9 ,KC_P0  ,KC_NO  ,
+     KC_NO  ,KC_NO  ,KC_P4 , KC_P5 , KC_P6  ,KC_NO  ,                  KC_PDOT, KC_P4 , KC_P5 , KC_P6 ,KC_NO  ,KC_NO  ,
+     KC_NO  ,KC_NO  ,KC_P1 , KC_P2 , KC_P3  ,KC_NO  ,                  KC_NO  , KC_P1 , KC_P2 , KC_P3 ,KC_PSLS,KC_NO  ,
+                     KC_NO , KC_NO ,                                                    KC_PLUS, KC_MINS,
                                              _______,                  _______,
-                                             _______,_______,  _______,KC_P0  ,
+                                             _______,_______,  _______,_______,
                                              _______,_______,  _______,_______
 
   ),
@@ -295,7 +297,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_MOUSE] = LAYOUT_5x6(
      _______,_______,_______,_______,_______,_______,                  _______,_______,_______,_______,_______,_______,
      _______,_______,_______,KC_MS_U,_______,_______,                  KC_BTN4,KC_BTN1,KC_BTN3,KC_BTN2,KC_BTN5,_______,
-     _______,_______,KC_MS_L,KC_MS_D,KC_MS_R,_______,                  KC_WH_L,KC_WH_D,KC_WH_U,KC_WH_R,_______,_______,
+     TG_MOU ,_______,KC_MS_L,KC_MS_D,KC_MS_R,_______,                  KC_WH_L,KC_WH_D,KC_WH_U,KC_WH_R,_______,_______,
      _______,_______,_______,_______,_______,_______,                  _______,_______,_______,_______,_______,_______,
                      _______,_______,                                                  _______,_______,
                                              _______,                  _______,
@@ -304,24 +306,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   ),
   [_GAME] = LAYOUT_5x6(
-     KC_1   , KC_2  , KC_3  , KC_4  , KC_5  , KC_6,                 KC_7, KC_8, KC_9,    KC_0,   KC_GRV,  RESET,
+     KC_1   , KC_2  , KC_3  , KC_4  , KC_5  , KC_6,                 KC_7, KC_8, KC_9,    KC_0,   KC_TILD, KC_GRV ,
      KC_TAB,  KC_Q  , KC_W  , KC_E  , KC_R  , KC_T,                 KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSLS,
      KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G,                 KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-     KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B,                 KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, OSM_LS,
-                      KC_LBRC, KC_RBRC,                                         KC_MINS, KC_EQL,
+     KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B,                 KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                      KC_LBRC, KC_RBRC,                                         KC_EQL,  KC_MINS,
                                       MO(_SIDE),                          OSM_LC,
                                       KC_SPC,KC_UNDS,          TG_MOU,    TS_ENT,
-                                      OSM_LA,TG_GAME,          OSM_WIN,   OSM_LA
+                                      OSM_LA,TG_GAME,          OSM_LW,   OSM_LA
   ),
-  [_CSGO] = LAYOUT_5x6(
-     KC_GRV, KC_5   , KC_1  , KC_2  , KC_3  , KC_4,                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-     KC_T,   KC_TAB,  KC_Q  , KC_W  , KC_E  , KC_R,                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-     KC_G,   KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F,                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-     KC_B,   KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V,                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                      KC_NO, KC_NO,                                               KC_NO, KC_NO,
+  [_WARBAND] = LAYOUT_5x6(
+     KC_GRV, KC_5   , KC_1  , KC_2  , KC_3  , KC_4,                 KC_NO, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO,
+     KC_T,   KC_TAB,  KC_Q  , KC_W  , KC_E  , KC_R,                 KC_NO, KC_F7 , KC_F8 , KC_F9 , KC_NO, KC_NO,
+     KC_G,   KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F,                 KC_NO, KC_F4 , KC_F5 , KC_F6 , KC_NO, KC_NO,
+     KC_B,   KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V,                 KC_NO, KC_F1 , KC_F2 , KC_F3 , KC_NO, KC_NO,
+                      KC_NO, KC_NO,                                                KC_NO, KC_NO,
                                       MO(_SIDE),                          KC_NO,
                                       KC_SPC ,KC_ESC,            KC_NO,   KC_NO,
-                                      KC_LALT,TG_CSGO,           KC_NO,   KC_NO
+                                      KC_LALT,TG_WARBAND,        KC_NO,   KC_NO
   ),
 };
 
